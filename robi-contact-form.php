62fbd98471faee3685d2de6560d3ad4ac5a21233
Robiul Islam <therobiulislam12@gmail.com>
2024-08-25 22:57:22 +0600

    Initial commit

---
+++ robi-contact-form.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Plugin Name:       Simple Contact Form
+ * Description:       Simple contact form plugin for learn ajax
+ * Plugin URI:        #
+ * Version:           1.0.0
+ * Author:            Robiul Islam
+ * Author URI:        https://robiul-islam.netlify.app
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Domain Path:       /languages
+ * Text Domain:      simple-contact-form
+ */
+
+ // check if anyone try to access our plugin
+if ( !defined( 'ABSPATH' ) ) {
+    header( 'Location: /' );
+    exit();
+}
